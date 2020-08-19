932bee6427f23fc726410e6fb61c81496a7889d1
ddyack <ddyack@ozon.ru>
2020-08-19 17:47:26 +0300

    Initial Commit

---
+++ trafficLightsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trafficLightsApp
+//
+//  Created by ddyack on 19.08.2020.
+//  Copyright © 2020 ddyack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
